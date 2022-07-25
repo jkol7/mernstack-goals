eee3defc7db96f26f19bcb293fa28c0dbb43cbc9
jkol7 <jkolcun7@gmail.com>
2022-07-25 13:39:46 -0400

    Controller and routes set up

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,17 @@
+const express = require('express')
+const dotenv = require('dotenv').config()
+const {errorHandler} = require('./middleware/errorMiddleware')
+
+
+const PORT = process.env.port || 5000
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+app.use(errorHandler)
+
+
+app.use('/api/goals', require('./routes/goalRoutes'))
+
+app.listen(PORT, () => console.log(`Server started on ${PORT}`))
 console.log('Hello world')
